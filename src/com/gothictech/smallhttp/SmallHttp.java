01e0eff23acbe0fcbe552c71b251d172fdbdb583
bobzilla <bobzilla@wigle.net>
2014-12-07 09:49:44 -0800

    Basic services endpoint

--- src/com/gothictech/smallhttp/SmallHttp.java
+++ src/com/gothictech/smallhttp/SmallHttp.java
@@ -1,12 +1,40 @@
+package com.gothictech.smallhttp;
 
-public class SmallHttp {
-  public static void main(String[] argv) {
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class SmallHttp extends Application<Config> {
+  public static void main(String[] args) {
     try {
-      Thread.sleep(60000);
+      new SmallHttp().run(args);
+      // Thread.sleep(60000);
     }
     catch (Exception ex) {
       System.out.println("ex: " + ex);
     }
+  }
+
+  @Override
+  public String getName() {
+    return "small-http";
+  }
+
+  @Override
+  public void initialize(Bootstrap<Config> bootstrap) {
+    // nothing to do yet
+  }
+
+  @Override
+  public void run(Config configuration, Environment environment) {
+    final Services resource = new Services(
+        configuration.getTemplate(),
+        configuration.getDefaultName()
+    );
+    final ServicesHealthCheck healthCheck =
+        new ServicesHealthCheck(configuration.getTemplate());
+    environment.healthChecks().register("template", healthCheck);
+    environment.jersey().register(resource);
   }
 
 }
