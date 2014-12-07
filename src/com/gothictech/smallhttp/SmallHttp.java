02ac5e77ff686397d3ebb139fdd3c2714e395b68
bobzilla <bobzilla@wigle.net>
2014-12-07 08:37:23 -0800

    initial commit

---
+++ src/com/gothictech/smallhttp/SmallHttp.java
@@ -0,0 +1,12 @@
+
+public class SmallHttp {
+  public static void main(String[] argv) {
+    try {
+      Thread.sleep(60000);
+    }
+    catch (Exception ex) {
+      System.out.println("ex: " + ex);
+    }
+  }
+
+}
